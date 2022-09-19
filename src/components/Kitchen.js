8cc9f43d1b6a6a4ad42e03bffee3c2845dd7253d
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-09-19 15:38:26 -0400

    completed kitchen js

---
+++ src/components/Kitchen.js
@@ -0,0 +1,15 @@
+import Oven from "./Oven";
+import Sink from "./Sink"
+ 
+ function Kitchen(props) {
+   return (
+     <div className="Kitchen">
+      <h3>Kitchen</h3>
+       <Oven/>
+       <Sink/>
+     </div>
+   );
+ }
+ 
+ // Must export the component's function (or class)
+ export default Kitchen;

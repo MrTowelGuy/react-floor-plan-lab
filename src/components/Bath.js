48b6f790f753b81936bddb988a0907a05530243a
Jaden Garrity <jaden@Jadens-MBP.attlocal.net>
2022-09-19 15:35:22 -0400

    completed bath.js

---
+++ src/components/Bath.js
@@ -0,0 +1,10 @@
+function Bath(props) {
+    return (
+      <div>
+        <h3>{props.whichBath} Bath</h3>
+      </div>
+    );
+  }
+  
+  // Must export the component's function (or class)
+  export default Bath;
